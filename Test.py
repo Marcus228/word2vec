c58f7fb906cc4eb978fcf8c6c547129f9ea434d0
Mark <60570844+Marcus228@users.noreply.github.com>
2026-03-04 17:47:58 +0000

    Experimental implementation of the algorithm, with hard-coded context and target word indecies.

--- Test.py
+++ Test.py
@@ -1,41 +1,54 @@
 import numpy as np
-
 import Tokeniser as tk
 import BinaryClassifiers as bcs
 
-t = tk.SimpleTokeniser()
 corpus = np.loadtxt("testText", dtype=str)
-tokens = t.tokeniseCorpus(corpus)
-print(corpus, corpus.shape)
-for cord in corpus:
-    print(cord)
+tokens = tk.tokeniseCorpus(corpus)
 print("dimension of corpus", corpus.shape)
 print("tokens size : ", len(tokens))
-print(tokens)
 
 embedding_dimension = 10
 # each row is a word embedding
-word_matrix = np.random.rand(len(tokens), embedding_dimension)
-context_matrix = np.random.rand(len(tokens), embedding_dimension)
+word_matrix = np.random.uniform(-0.5, 0.5, (len(tokens), embedding_dimension))
+context_matrix = np.random.uniform(-0.5, 0.5, (len(tokens), embedding_dimension))
 # context_matrix = np.zeroes(tokens)
-print(word_matrix, word_matrix.shape)
-print(context_matrix, context_matrix.shape)
+print("Word matrix", word_matrix)
+print("Word matrix shape", word_matrix.shape)
+print("Context matrix shape", context_matrix.shape)
+epochs = 100
 
-# experimenting with numpy broadcasting feature
-# using broadcasting can technically allow me to do all the dot product computations at once
-# choose an arbitrary word
-word_index = 2
-context_index1 = word_index + 5
-context_index2 = word_index - 1
-print("The word is",word_matrix[word_index])
-print("The contexts are",context_matrix[context_index1])
-# extend the original word embedding to the context embedding
-brdcstd_arrs = np.broadcast_arrays(word_matrix[word_index], [context_matrix[context_index1], context_matrix[context_index2]])
-print("The broadcasted word_embedding is", brdcstd_arrs[0])
-print("The original word_contexts are", brdcstd_arrs[1])
-# map a dot product over the broadcasted word_embedding and word_contexts
-dot_product = np.array([np.dot(word, context) for word, context in zip(brdcstd_arrs[0], brdcstd_arrs[1])])
-print("The dot product is", dot_product)
-# map sigmoid function over the dot product matrix
-sigmoid = np.array([bcs.sigmoid(dot) for dot in dot_product])
-print(sigmoid)
+while epochs > 0:
+    # experimenting with numpy broadcasting feature
+    # using broadcasting can technically allow me to do all the dot product computations at once
+    # choose an arbitrary word
+    word_index = tokens["term"]
+    context_index1 = word_index + 5
+    context_index2 = word_index - 1
+    print("The word is",word_matrix[word_index])
+    print("The contexts are",  [context_matrix[context_index1], context_matrix[context_index2]])
+
+    # extend the original word embedding to the context embedding
+    brdcstd_arrs = np.broadcast_arrays(word_matrix[word_index], [context_matrix[context_index1], context_matrix[context_index2]])
+    print("The broadcasted word_embedding is", brdcstd_arrs[0])
+    print("The original word_contexts are", brdcstd_arrs[1])
+
+    # map a dot product over the broadcasted word_embedding and word_contexts
+    dot_product = np.array([np.dot(word, context) for word, context in zip(brdcstd_arrs[0], brdcstd_arrs[1])])
+    print("The dot products are", dot_product)
+
+    # map sigmoid function over the dot product matrix
+    sigmoid = np.array([bcs.sigmoid(dot) for dot in dot_product])
+    print("Classification scores are", sigmoid)
+
+    # calculate the loss and gradient
+    learning_rate = 0.025
+    errors = np.array([0,1]) - sigmoid
+    print("Loss is", errors)
+    gradient = errors[:, np.newaxis] * word_matrix[word_index]
+    print("Gradient is", gradient)
+
+    # update the word embeddings
+    print("The original word embedding is", word_matrix[word_index])
+    np.add.at(word_matrix, [word_index, word_index], gradient * learning_rate)
+    print("Updated word embedding is", word_matrix[word_index])
+    epochs -= 1
--- Tokeniser.py
+++ Tokeniser.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-class SimpleTokeniser:
-    def tokeniseCorpus(self, corpus: np.ndarray) -> dict:
-        tokens = {}
-        counter = 0
-        for sentence in corpus:
-            for word in sentence.split():
-                if word not in tokens:
-                    tokens.update({word : counter + 1})
-                    counter += 1
-        return tokens
+
+def tokeniseCorpus(corpus: np.ndarray) -> dict:
+    tokens = {}
+    counter = 0
+    for sentence in corpus:
+        for word in sentence.split():
+            if word not in tokens:
+                tokens.update({word : counter + 1})
+                counter += 1
+    return tokens
